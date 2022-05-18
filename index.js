742a1fb2effd7d89fd2e827354ae12900b9fcf12
Antoine Lolliot <antoinelolliot@gmail.com>
2022-05-17 21:12:05 -0500

    created playable game, including button calcs and scoring

--- index.js
+++ index.js
@@ -1,50 +1,106 @@
-let playerTotal = 0
-let computerTotal = 0
+playerTotal = 0
+computerTotal = 0
+tieTotal = 0
 
-function computerplay() {
+function computerPlay() {
     let choices = ["Rock","Paper","Scissors"]
     let randomNum = Math.floor(Math.random()*3);
-    let computerChoice = choices[randomNum];
+     computerChoice = choices[randomNum];
     return computerChoice;
 }
-function playerPick() {
-    let playerInput = prompt("Rock,Paper,Scissors?");
-    let playerChoice =  playerInput.slice(0,1).toUpperCase()+ playerInput.slice(1,playerInput.length).toLowerCase();
-    return playerChoice;
-}
-function playRound(playerChoice,computerChoice) {
 
+// function playerPick() {
+//     let playerInput = prompt("Rock,Paper,Scissors?");
+//     let playerChoice =  playerInput.slice(0,1).toUpperCase()+ playerInput.slice(1,playerInput.length).toLowerCase();
+//     return playerChoice;
+// }
+
+
+function displayResult(result) {
+    const container = document.querySelector('#container');
+        const content = document.createElement('div');
+        content.classList.add('content');
+        content.textContent = result;
+        container.appendChild(content);
+}
+
+function displayScore(score) {
+    const container = document.querySelector('#container');
+        const gameScore = document.createElement('div');
+        gameScore.classList.add('gameScore');
+        gameScore.textContent = `Player Score: ${playerTotal} Computer Score: ${computerTotal} Tie Total: ${tieTotal}`;
+        container.appendChild(gameScore);
+}
+
+function displayVictor() {
+    determineWinner();
+    const container = document.querySelector('#container');
+        const victor = document.createElement('div');
+        victor.classList.add('victor');
+        victor.textContent = winnerStatement;
+        container.appendChild(victor);
+}
+
+function playRound(playerChoice,computerChoice) {
+    
     if (computerChoice === playerChoice) {
-        console.log("You Tied!");
+        ++tieTotal;
+        let result = 'You Tied!';
+        displayResult(result);
+
     } else if (computerChoice === "Rock" && playerChoice === "Scissors"){
         ++computerTotal;
-        console.log( "You Lose!")
+        let result = 'You Lose!';
+        displayResult(result);
     
     } else if (computerChoice === "Scissors" && playerChoice === "Paper") {
         ++computerTotal;
-        console.log("You Lose!")
+        let result = 'You Lose!';
+        displayResult(result);
+
     } else if (computerChoice === "Paper" && playerChoice === "Rock") {
         ++computerTotal;
-        console.log("You Lose!")
+        const div = document.createElement('div');
+        let result = 'You Lose!';
+        displayResult(result);
     } else {
         ++playerTotal;
-        console.log("You win!")
+        let result = 'You Win!';
+        displayResult(result);
     }
+    displayScore(playerTotal,computerTotal);
 }
 
-function game() {
-    for (let i=0;i<5;i++) {
-const playerSelection = playerPick();
-const computerSelection = computerplay();
-console.log(playRound(playerSelection, computerSelection));
-}
-if (playerTotal < computerTotal) {
-    console.log( "you lost to the robot")
-} else if (playerTotal === computerTotal) {
-    return "You tied the robot"
-} else {
-    return "You beat the robot"
-}
-}
+let gamesPlayed = 0;
+     buttons = document.querySelectorAll('button');
+    buttons.forEach((button)=> {
+    button.addEventListener('click', () => {
+         playerChoice = button.id;
+         computerPlay();
+         playRound(playerChoice,computerChoice);
+         gamesPlayed+=1;
+         if (gamesPlayed >= 5) {
+            buttons.forEach(button => {
+                 button.setAttribute("disabled",true);
+                 
+             });
+            displayVictor();
+         }
+    
+    });
+   
+});
 
-console.log(game());
+function determineWinner (){
+    if (playerTotal === computerTotal) {
+         winnerStatement = "You tied the computer!";
+        return winnerStatement;
+    } else if (playerTotal > computerTotal) {
+         winnerStatement = "You beat the computer!"
+        return winnerStatement;
+    }else {
+         winnerStatement = "The computer Wins!"
+        return winnerStatement;
+    }
+
+}
